6eb246fdf59c7d5497b2566f1b6f2564d7ed3063
taojy123 <taojy123@163.com>
2018-09-12 15:16:07 +0800

    fix

--- athenatools/urls.py
+++ athenatools/urls.py
@@ -47,14 +47,14 @@ urlpatterns = [
 
 
 # This will work if DEBUG is True
-# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-# urlpatterns += staticfiles_urlpatterns()
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
 
 # This will work if DEBUG is True or False
-from django.conf import settings
-from django.views.static import serve
-import re
-urlpatterns.append(url(
-    '^' + re.escape(settings.STATIC_URL.lstrip('/')) + '(?P<path>.*)$',
-    serve,
-    {'document_root': './static/'}))
+# from django.conf import settings
+# from django.views.static import serve
+# import re
+# urlpatterns.append(url(
+#     '^' + re.escape(settings.STATIC_URL.lstrip('/')) + '(?P<path>.*)$',
+#     serve,
+#     {'document_root': './static/'}))
