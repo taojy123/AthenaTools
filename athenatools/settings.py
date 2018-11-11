14f3c71b1136d1f53c3880a73a38f20f03b34feb
taojy123 <taojy123@163.com>
2018-11-11 20:44:13 +0800

    update database

--- athenatools/settings.py
+++ athenatools/settings.py
@@ -21,13 +21,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'data.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        # 'OPTIONS': {'charset': 'utf8mb4'}, # for emoji at mysql
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'athenatools',                      # Or path to database file if using sqlite3.
+        'USER': 'athenatools',                      # Not used with sqlite3.
+        'PASSWORD': 'athenatools',                  # Not used with sqlite3.
+        'HOST': 'tools.athenagu.com',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
+        'OPTIONS': {'charset': 'utf8mb4'},   # for emoji at mysql
     }
 }
 
